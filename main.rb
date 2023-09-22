a3a94ce4cae03da70caa06e3ab71340a44f6cb8a
Yuli Wang <yuliwang1007@gmail.com>
2023-09-22 11:28:24 -0400

    changed to require

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
-require_relative "./player"
-require_relative "./questions"
-require_relative "./games"
+require "./player"
+require "./questions"
+require "./games"
 
 new_game = Games.new("New game")
 new_game.start
